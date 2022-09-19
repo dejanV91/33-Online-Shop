234ccf834883fca2d332db178bbe288e623cab37
dejanV91 <dejanvukoicic91@gmail.com>
2022-09-19 21:29:59 +0200

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 'use strict';
 
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-function buildStyles() {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
-};
+});
 
-exports.buildStyles = buildStyles;
-exports.watch = function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-};
+gulp.task('sass:watch', function () {
+    gulp.watch( './sass/**/*.scss');
+})
