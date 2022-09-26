71748004677faf325d936b7c5e1564fbb508c80b
dejanV91 <dejanvukoicic91@gmail.com>
2022-09-26 20:58:42 +0200

    initial commit

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,18 @@ jQuery(document).ready(function($) {
     $("#search-btn").click( function(e){
         e.preventDefault();
 
-        $(".search-wrraper").show();
+        $(".search-wrraper").fadeIn()
     });
 
     $("#close-search").click(function() {
-        $(".search-wrraper").hide();        
+        $(".search-wrraper").fadeOut();        
+    })
+
+    $(window).scroll( function(){
+        if ($(window).scrollTop() > 100) {
+            $("nav").addClass("fixed-nav");
+        }else{
+            $("nav").removeClass("fixed-nav");
+        }
     })
 })
