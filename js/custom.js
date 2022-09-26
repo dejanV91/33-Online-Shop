f3a04b9aad52b599d564642a0a9091bde2de48dd
dejanV91 <dejanvukoicic91@gmail.com>
2022-09-26 19:51:19 +0200

    initial commit

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,11 @@
 jQuery(document).ready(function($) {
-    
+    $("#search-btn").click( function(e){
+        e.preventDefault();
+
+        $(".search-wrraper").show();
+    });
+
+    $("#close-search").click(function() {
+        $(".search-wrraper").hide();        
+    })
 })
