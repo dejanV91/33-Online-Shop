b80525ea50e216f0370b7b279f5ffdae22189911
dejanV91 <dejanvukoicic91@gmail.com>
2022-09-28 23:01:32 +0200

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
-let xhttp = new XMLHttpRequest;
-console.log(xhttp);
+let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
-        
+        console.log(this.responseText);
+
+        let obj = JSON.parse(this.responseText)
+        console.log(obj);
+
     }
 }
+
+xhttp.open("GET","https://6334a608ea0de5318a06d722.mockapi.io/products", true);
+xhttp.send();
