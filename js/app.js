c2750a81e1c96f62d2576da456501e9e70a35f31
dejanV91 <dejanvukoicic91@gmail.com>
2022-09-29 21:54:16 +0200

    initial commit

--- js/app.js
+++ js/app.js
@@ -18,7 +18,9 @@ xhttp.onreadystatechange = function(){
                             <h5 class="card-title">${name}</h5>
                             <p class="card-text">$${price}</p>
                             <button class="btn btn-primary">Add To Cart</button>
-                            <button onclick="seeMore(this)" data-product-id="${id}" class="btn btn-info">See More</button>
+                            <button onclick="seeMore(this)" data-product-id="${id}" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#seeMoreModal">
+                                See More
+                            </button>
                         </div>
                     </div>`
         })
@@ -33,4 +35,18 @@ xhttp.send();
 
 function seeMore(el){
     let id = el.dataset.productId;
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            
+            let obj = JSON.parse(this.responseText);
+            
+            
+        }
+    }
+
+    xhttp.open("GET","https://6334a608ea0de5318a06d722.mockapi.io/products/" + id, true);
+    xhttp.send();
+
 }
