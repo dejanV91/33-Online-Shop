e7029c64df5f7cbba044a8a35ddfd4bf3ff67474
dejanV91 <dejanvukoicic91@gmail.com>
2022-09-29 21:12:44 +0200

    initial commit

--- js/app.js
+++ js/app.js
@@ -2,21 +2,28 @@ let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
 
-        let obj = JSON.parse(this.responseText)
+        let obj = JSON.parse(this.responseText);
         let productsEl = document.getElementById("products");
-        let html = "";
+        
+        const datas = obj.map(function(item){
+            const {product_description : desc, 
+                    product_image : img, 
+                    product_name : name, 
+                    product_price : price} = item;
+           
+            return `<div class="card" style="width: 18rem; margin:10px; padding:10px;">
+                        <img class="card-img-top" src="${img}" alt="${name}">
+                        <div class="card-body">
+                            <h5 class="card-title">${name}</h5>
+                            <p class="card-text">$${price}</p>
+                            <button class="btn btn-primary">Add To Cart</button>
+                            <button class="btn btn-info">See more</button>
+                        </div>
+                    </div>`
+        })
+        .join("");
 
-        for (let i = 0; i < obj.length ; i++) {
-            html += "<div class='col-md-4'>" +
-            "<div class='card'>"+
-            "<img src="+obj[i].product_image+">"+
-           " </div>"+
-            "</div>"
-            
-        }
-
-        productsEl.innerHTML = html;
-
+        productsEl.innerHTML = datas;
     }
 }
 
